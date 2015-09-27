b8cc2a4d02b1826b5269dab67098ac1206c86994
Peter Zarafield <Peter Zarafield@Tyson.attlocal.net>
2015-09-27 18:55:19 -0500

    Lab DIP
    Second Commit
    Creating the basics of the "message" object/class and writing everything down the old fashioned way on paper with flowcharts, etc.

--- LabDIPMessagingProgram/src/labdipmessagingprogram/Message.java
+++ LabDIPMessagingProgram/src/labdipmessagingprogram/Message.java
@@ -16,4 +16,40 @@ public class Message {
     private int intMessage;
     private double doubleMessage;
     
+    // future data types
+    // private double audio files
+    // private double text file files
+    // private double image files
+    
+    public Message (String msg){
+        this.stringMessage = msg;
+    }
+    
+    public  Message (int msg) {
+        this.intMessage = msg;
+    }
+    
+    public Message (double msg){
+        this.doubleMessage = msg;
+    }
+
+    
+    // Getters for the different types of message
+    // Initial tests have no setters for the types of messages
+    // This forces the programmer to use the constructor, creating a new
+    // instance of the "message" class for each message created
+    public String getStringMessage() {
+        return stringMessage;
+    }
+
+    public int getIntMessage() {
+        return intMessage;
+    }
+
+    public double getDoubleMessage() {
+        return doubleMessage;
+    }
+    
+    
+    
 }
