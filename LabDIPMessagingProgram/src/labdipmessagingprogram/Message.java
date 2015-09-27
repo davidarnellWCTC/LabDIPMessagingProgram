de4afb41cfe8c65a70b56f0439c1249ccaa3752e
Peter Zarafield <Peter Zarafield@Tyson.attlocal.net>
2015-09-27 17:43:27 -0500

    Lab DIP
    Initial Commits
    Creation of the Message class

---
+++ LabDIPMessagingProgram/src/labdipmessagingprogram/Message.java
@@ -0,0 +1,19 @@
+package labdipmessagingprogram;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Peter Zarafield
+ */
+public class Message {
+    
+    private String stringMessage;
+    private int intMessage;
+    private double doubleMessage;
+    
+}
